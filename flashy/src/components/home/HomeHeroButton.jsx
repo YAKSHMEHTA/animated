3ee13c751877b9457b252ddde4fa67d59a2fe835
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2025-10-17 10:20:55 +0530

    Corrected commit for contribution graph

--- flashy/src/components/home/HomeHeroButton.jsx
+++ flashy/src/components/home/HomeHeroButton.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import Video from './Video'
+import React from "react";
+import Video from "./Video";
 
 const HomeHeroButton = () => {
   return (
-    <div className='font-[font2] pt-5 text-center' >
-      <div className='text-[8vw] uppercase leading-[6vw]  flex justify-center items-center' >The spark</div>
-      <div className='text-[8vw] uppercase leading-[7vw] flex gap-6 justify-center items-center  ' >who <div className='h-[7.5vw] w-[16vw] rounded-full overflow-hidden mb-9' loop  ><Video /></div></div>
-      <div className='text-[8vw] uppercase leading-[6vw]  flex justify-center items-center' >Generates</div>
-      <div className='text-[8vw] uppercase leading-[7.5vw] flex justify-center items-center' >thecreativity</div>
+    <div className="font-[font2] pt-5 text-center">
+      <div className="text-[8vw] uppercase leading-[6vw]  flex justify-center items-center">
+        The spark
+      </div>
+      <div className="text-[8vw] uppercase leading-[7vw] flex gap-6 justify-center items-center  ">
+        who{" "}
+        <div
+          className="h-[7.5vw] w-[16vw] rounded-full overflow-hidden mb-9"
+          loop
+        >
+          <Video />
+        </div>
+      </div>
+      <div className="text-[8vw] uppercase leading-[6vw]  flex justify-center items-center">
+        Generates
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default HomeHeroButton
+export default HomeHeroButton;
