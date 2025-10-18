cb5468e31d3c9441a241ae53b4de478643b6f49f
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2025-10-18 16:48:47 +0530

    Agence page fix

--- flashy/src/pages/Agence.jsx
+++ flashy/src/pages/Agence.jsx
@@ -10,6 +10,7 @@ const Agence = () => {
   const imgRef = useRef(null);
   const imgarr = [
     "https://i.pinimg.com/1200x/d4/4a/75/d44a755453ff82c65cc47a320f9fdf18.jpg",
+    "https://i.pinimg.com/1200x/d4/4a/75/d44a755453ff82c65cc47a320f9fdf18.jpg",
     "https://i.pinimg.com/474x/88/38/4f/88384f3c7267ec1c0ac3755e485d2cb3.jpg",
     "https://i.pinimg.com/736x/07/81/77/0781770279c78e2290aa622d01fa9541.jpg",
     "https://i.pinimg.com/736x/2a/85/13/2a8513396e5c47e57c4a7eaafb923570.jpg",
@@ -27,45 +28,47 @@ const Agence = () => {
     gsap.to(imgDivRef.current, {
       scrollTrigger: {
         trigger: imgDivRef.current,
-        markers: true,
         start: "top 26%",
-        end: "top -120%",
+        end: "top -170%",
         scrub: true,
         pin: true,
-        onUpdate:(elem)=>{
-          const imgIndex = Math.floor(elem.progress * (imgarr.length-1))
-          imgRef.current.src = imgarr[imgIndex];
-        }
+        onUpdate: (elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imgarr.length)
+          } else {
+            imageIndex = imgarr.length - 1
+          }
+          imgRef.current.src = imgarr[imageIndex]
+        },
       },
     });
   });
 
   return (
-    <div>
-      <div className="section1">
+    <div className="parent">
+      <div id="page1" className="py-1 ">
         <div
           ref={imgDivRef}
-          className="absolute h-[18vw] rounded-3xl overflow-hidden w-[14vw] top-46 left-[30vw] bg-amber-950 "
+          className=
+"absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-[-48] -top[-45] lg:left-[30vw] left-[30vw]"
         >
           <img
             ref={imgRef}
-            className="h-full w-full object-cover"
-            src=""
+            className="h-full object-cover w-full"
+            src="https://i.pinimg.com/1200x/d4/4a/75/d44a755453ff82c65cc47a320f9fdf18.jpg"
             alt=""
           />
         </div>
-        <div className="font-[font2] relative">
-          <div className="mt-[55vh] ">
-            <div>
-              <h2 className="text-[19vw] text-center uppercase leading-[17vw]">
-                midnight
-                <br />
-                <h2 className="pl-39">sparks</h2>
-              </h2>
-            </div>
+        <div className="relative font-[font2]">
+          <div className="lg:mt-[55vh] mt-[30vh]">
+            <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
+              midnight <br />
+              sparks
+            </h1>
           </div>
-          <div className="pl-[40%] mt-20">
-            <p className="text-5xl w-2/3">
+          <div className="lg:pl-[40%] lg:mt-20 mt-4 p-3">
+            <p className="lg:text-6xl text-xl leading-tight">
               {" "}
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Unleash the
               fizz, feel the buzz! Dive into the world where crisp refreshment
@@ -76,9 +79,7 @@ const Agence = () => {
           </div>
         </div>
       </div>
-      <div className="section2 h-screen">
-        
-      </div>
+      <div id="page2" className=" h-screen"></div>
     </div>
   );
 };
